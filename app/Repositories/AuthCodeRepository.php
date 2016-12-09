70d8fd6026e1cd252054e4f3fdcc6a543f0c7130
agrim mittal <agrimmittal97@gmail.com>
2016-12-09 22:29:57 +0530

    add create function for auth code

--- app/Repositories/AuthCodeRepository.php
+++ app/Repositories/AuthCodeRepository.php
@@ -1,7 +1,8 @@
 <?php
- 
+
 
 namespace Oauth\Server\Repositories;
+use Oauth\Server\Model\Database;
 
 use League\OAuth2\Server\Entities\AuthCodeEntityInterface;
 use League\OAuth2\Server\Repositories\AuthCodeRepositoryInterface;
@@ -11,7 +12,7 @@ class AuthCodeRepository implements AuthCodeRepositoryInterface
 {
     /**
      * {@inheritdoc}
-     */
+    **/
     public function persistNewAuthCode(AuthCodeEntityInterface $authCodeEntity)
     {
         // Some logic to persist the auth code to a database
@@ -39,5 +40,13 @@ class AuthCodeRepository implements AuthCodeRepositoryInterface
     public function getNewAuthCode()
     {
         return new AuthCodeEntity();
+    }
+    public function create($auth_code, $expire_time, $session_id, $redirect_uri)
+    {
+        $query = Database::get_instance()->prepare(
+            "INSERT INTO `oauth_auth_codes`(auth_code, session_id, expire_time, client_redirect_uri)
+            VALUES(:auth_code, :session_id, :expire_time, :client_redirect_uri)");
+
+        $query->execute(array("auth_code" => $auth_code, "expire_time" => $expire_time, "session_id", $session_id, "client_redirect_uri" => $redirect_uri));
     }
 }
